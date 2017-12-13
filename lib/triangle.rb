893805be12226fff176edf230a1727f13f9b137f
Rebecca Tong <rtong@buffalo.edu>
2017-12-13 04:11:03 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
 
   def kind
     raise TriangleError if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
-    raise TriangleError if (@length1 + @length2) < @length3 || (@length2 + @length3) < @length1 || @
+    raise TriangleError if (@length1 + @length2) < @length3 || (@length2 + @length3) < @length1 || (@length3 + @length1) < @length2
 
     if @length1 == @length2 && @length2 == @length3
       return :equilateral
