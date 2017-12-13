deee4e69376ca038d48dc1f362ae74a0dac7e41d
Rebecca Tong <rtong@buffalo.edu>
2017-12-13 03:41:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
     elsif @length1 != @length2 && @length2 != @length3
       return :scalene
     elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
+      return :isosceles
 
     end
   end
