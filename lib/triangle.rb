2c530c9afba98e6b8be2b30c3ef00218678e4bcf
Rebecca Tong <rtong@buffalo.edu>
2017-12-13 03:42:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,10 +11,10 @@ class Triangle
   def kind
     if @length1 == @length2 && @length2 == @length3
       return :equilateral
-    elsif @length1 != @length2 && @length2 != @length3
-      return :scalene
     elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
       return :isosceles
+    elsif @length1 != @length2 && @length2 != @length3
+      return :scalene
 
     end
   end
