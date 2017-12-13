30b901e0d6f02a1f9dd6f1212c063323fb47fe98
Rebecca Tong <rtong@buffalo.edu>
2017-12-13 03:36:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral,:isosceles,:scalene
+
+  def initialize(length1,length2,length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if @length1 == @length2 == @length3
+  end
+
+end
+
+
+class TriangleError < StandardError
+ # triangle error code
 end
